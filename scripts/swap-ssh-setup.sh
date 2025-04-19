41340bf2a344c30d6bf9d44e11f0a16a10286c33
brianharte <77025448+brianharte@users.noreply.github.com>
2025-04-19 20:08:07 +0100

    Update swap-ssh-setup.sh
    commented out swap generation as not currently possible

--- scripts/swap-ssh-setup.sh
+++ scripts/swap-ssh-setup.sh
@@ -1,25 +1,27 @@
+# Note creating a dedicated swap partiton at boot isnt working so I have commented it out for now. Leaving the code in case I figure it out
+
 # Creates swap space on the two drives /dev/vda /dev/vdb and set them up for Concurrent usage
 
 #!/usr/bin/bash
 set -e
 
-# Set up swap
-
-for d in /dev/vda /dev/vdb; do
-  # Get the number of the next available partition (e.g., 3)
-  nextpart=$(parted --script $d print | awk '/^ /{print $1}' | tail -n1)
-  nextpart=$((nextpart + 1))
-
-  # Use all remaining space for swap
-  parted --script $d mkpart primary linux-swap 100%FREE
-
-  # Build the new partition path
-  p="${d}${nextpart}"
-  
-  mkswap $p
-  swapon $p
-  echo "$p none swap sw 0 0" >> /etc/fstab
-done
+## Set up swap
+#
+#for d in /dev/vda /dev/vdb; do
+#  # Get the number of the next available partition (e.g., 3)
+#  nextpart=$(parted --script $d print | awk '/^ /{print $1}' | tail -n1)
+#  nextpart=$((nextpart + 1))
+#
+#  # Use all remaining space for swap
+#  parted --script $d mkpart primary linux-swap 100%FREE
+#
+#  # Build the new partition path
+#  p="${d}${nextpart}"
+#  
+#  mkswap $p
+#  swapon $p
+#  echo "$p none swap sw 0 0" >> /etc/fstab
+#done
 
 # Set up SSH
 
